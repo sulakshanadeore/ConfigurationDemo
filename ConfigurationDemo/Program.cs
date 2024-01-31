f4a38c076e8565edd80b4e3f81663e5f1d58faaa
deoresulakshana <deoresulakshana@gmail.com>
2024-01-31 18:11:41 +0530

    Add project files.

---
+++ ConfigurationDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using DemoConfig;
+namespace ConfigurationDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int TotalPlayers=ReadConfigFile.GetPlayerCount();
+            Console.WriteLine(TotalPlayers);
+
+            //string value=ConfigurationManager.AppSettings.Get("p1");
+            //Console.WriteLine(value);
+
+            string value=ConfigurationManager.AppSettings["p1"];
+            Console.WriteLine(value);
+            Console.ReadLine();
+
+        }
+
+        
+    }
+}
---
+++ DemoConfig/ReadConfigFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace DemoConfig
+{
+    public class ReadConfigFile
+    {
+        public static int GetPlayerCount()
+        {
+            int CountPlayers = Convert.ToInt32(ConfigurationManager.AppSettings["NumberOfPlayers"]);
+            return CountPlayers;
+
+        }
+    }
+}
